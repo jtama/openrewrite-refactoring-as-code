8083e345f18a09c3ec8bb814b975652dc5870adb
jtama <jerome.tama@gmail.com>
2025-09-26 08:48:49 +0200

    Better theming

--- setup.sh
+++ setup.sh
@@ -7,3 +7,4 @@ rm -rf reveal.js/plugin/menu/.git
 cd reveal.js && npm i && npm run build && cd ../
 mkdir highlight.js
 curl -sLo highlight.js/highlight.min.js https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.7.3/highlight.min.js
+curl -sLo highlight.js/atom-one-light.min.css https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.7.3/styles/atom-one-light.min.css
